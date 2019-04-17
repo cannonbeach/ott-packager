1a85c9c2ba90223b47ee0ee1ba14288714fa0609
cannonbeach <cannonbeachgoonie@gmail.com>
2019-04-17 18:28:42 -0500

    Continuing to build NodeJS web interface

--- webapp/public/client.js
+++ webapp/public/client.js
@@ -1,70 +1,196 @@
 console.log('Client-side code running');
 document.getElementById("newsourcepage").style.display = "none";
 
-const start_button = document.getElementById('new_button');
-start_button.addEventListener('click', function(e) {
-    console.log('new button was clicked');
-    var button = document.getElementById('new_button');
-    button.disabled = true;
+var new_button = document.getElementById('new_button');
+var start_button = document.getElementById('start_button1');
+var reset_button = document.getElementById('reset_button1');
+var stop_button1 = document.getElementById('stop_button1');
+var submit_button = document.getElementById('submit_button');
+var abort_button = document.getElementById('abort_button');
 
-    document.getElementById("controlpage").style.display = "none";
-    document.getElementById("statuspage").style.display = "none";
-    document.getElementById("newsourcepage").style.display = "block";
-    
-    button.disabled = false;
-});
+if (new_button) {
+    new_button.addEventListener('click', function(e) {
+	console.log('new button was clicked');
+	var button = document.getElementById('new_button');
+	button.disabled = true;
+	
+	document.getElementById("controlpage").style.display = "none";
+	document.getElementById("statuspage").style.display = "none";
+	document.getElementById("newsourcepage").style.display = "block";
+	
+	button.disabled = false;
+    });
+}
 
-const stop_button = document.getElementById('stop_button');
-stop_button.addEventListener('click', function(e) {
-    console.log('stop button was clicked');
-    var button = document.getElementById('stop_button');
-    button.disabled = true;
+if (reset_button) {
+    reset_button.addEventListener('click', function(e) {
+	console.log('reset button was clicked');
+	var button = document.getElementById('reset_button');
+	button.disabled = true;
+	
+	fetch('/reset_clicked',{method: 'POST'})
+	    .then(function(response) {
+		if (response.ok) {
+		    console.log('reset clicked confirmed');
+		    return;
+		}
+		throw new Error('Reset request failed.');
+	    })
+	    .catch(function(error) {
+		console.log(error);
+	    });
+	
+    });
+}
 
-    fetch('/stop_clicked',{method: 'POST'})
-	.then(function(response) {
-	    if (response.ok) {
-		console.log('stop clicked confirmed');
-		return;
-	    }
-	    throw new Error('Stop request failed.');
-	})
-	.catch(function(error) {
-	    console.log(error);
-	});
+if (stop_button1) {
+    stop_button1.addEventListener('click', function(e) {
+	console.log('stop button was clicked');
+	var button = document.getElementById('stop_button1');
+	button.disabled = true;
+	
+	fetch('/stop_clicked',{method: 'POST'})
+	    .then(function(response) {
+		if (response.ok) {
+		    console.log('stop clicked confirmed');
+		    return;
+		}
+		throw new Error('Stop request failed.');
+	    })
+	    .catch(function(error) {
+		console.log(error);
+	    });
+	
+    });
+}
 
-});
-
-const reset_button = document.getElementById('reset_button');
-reset_button.addEventListener('click', function(e) {
-    console.log('reset button was clicked');
-    var button = document.getElementById('reset_button');
-    button.disabled = true;
-
-    fetch('/reset_clicked',{method: 'POST'})
-	.then(function(response) {
-	    if (response.ok) {
-		console.log('reset clicked confirmed');
-		return;
-	    }
-	    throw new Error('Reset request failed.');
-	})
-	.catch(function(error) {
-	    console.log(error);
-	});
-
-});
-
-const submit_button = document.getElementById('submit_button');
 submit_button.addEventListener('click', function(e) {
     console.log('submit button was clicked');
+
+    var sourcename = document.getElementById("sourcename").value;
+    var ipaddr_primary = document.getElementById("ipaddr_primary").value;
+    var inputinterface1 = document.getElementById("inputinterface1").value;    
+    var ipaddr_backup = document.getElementById("ipaddr_backup").value;
+    var inputinterface2 = document.getElementById("inputinterface2").value;
+    var enablehls = document.getElementById("enablehls").value;
+    var enabledash = document.getElementById("enabledash").value;
+    var windowsize = document.getElementById("windowsize").value;
+    var segmentsize = document.getElementById("segmentsize").value;
+    var enablescte35 = document.getElementById("enablescte35").value;
+    var videocodec = document.getElementById("videocodec").value;
+    var audiosources = document.getElementById("audiosources").value;
+    var enablestereo = document.getElementById("enablestereo").value;
+    var audiobitrate = document.getElementById("audiobitrate").value;
     
-    document.getElementById("controlpage").style.display = "block";
-    document.getElementById("statuspage").style.display = "block";
-    document.getElementById("newsourcepage").style.display = "none";
-    alert("Saved!");
+    var outputenabled1 = document.getElementById("outputenabled1").value;
+    var outputresolution1 = document.getElementById("outputresolution1").value;
+    var video_bitrate1 = document.getElementById("video_bitrate1").value;
+    
+    var outputenabled2 = document.getElementById("outputenabled2").value;    
+    var outputresolution2 = document.getElementById("outputresolution2").value;
+    var video_bitrate2 = document.getElementById("video_bitrate2").value;
+    
+    var outputenabled3 = document.getElementById("outputenabled3").value;
+    var outputresolution3 = document.getElementById("outputresolution3").value;
+    var video_bitrate3 = document.getElementById("video_bitrate3").value;
+    
+    var outputenabled4 = document.getElementById("outputenabled4").value;
+    var outputresolution4 = document.getElementById("outputresolution4").value;
+    var video_bitrate4 = document.getElementById("video_bitrate4").value;
+
+    var hlsmanifest = document.getElementById("hlsmanifest").value;
+    var fmp4manifest = document.getElementById("fmp4manifest").value;
+    var dashmanifest = document.getElementById("dashmanifest").value;
+    var publishpoint1 = document.getElementById("publishpoint1").value;
+    var cdnusername1 = document.getElementById("cdnusername1").value;
+    var cdnpassword1 = document.getElementById("cdnpassword1").value;
+    
+    var safe = 1;
+    
+    // check that fields are alright before continuing
+    var windowsize_int = parseInt(windowsize);   
+    if (isNaN(windowsize_int)) {
+	safe = 2;
+	console.log('windowsize is NaN');
+    } else if (windowsize_int <= 0 || windowsize_int >= 30) {
+	safe = 2;
+	console.log('windowsize is outside of limits');
+    }
+    
+    if (safe == 1) {
+	document.getElementById("controlpage").style.display = "block";
+	document.getElementById("statuspage").style.display = "block";
+	document.getElementById("newsourcepage").style.display = "none";
+	
+	var obj = new Object();
+	obj.sourcename = sourcename;	
+	obj.ipaddr_primary = ipaddr_primary;
+	obj.inputinterface1 = inputinterface1;
+	obj.ipaddr_backup = ipaddr_backup;
+	obj.inputinterface2 = inputinterface2;
+	
+	obj.enablehls = enablehls;
+	obj.enabledash = enabledash;
+	obj.windowsize = windowsize;
+	obj.segmentsize = segmentsize;
+
+	obj.enablescte35 = enablescte35;
+	obj.videocodec = videocodec;
+	obj.audiosources = audiosources;
+	obj.enablestereo = enablestereo;
+	obj.audiobitrate = audiobitrate;
+
+	obj.outputenabled1 = outputenabled1;
+	obj.outputresolution1 = outputresolution1;
+	obj.video_bitrate1 = video_bitrate1;
+
+	obj.outputenabled2 = outputenabled2;
+	obj.outputresolution2 = outputresolution2;
+	obj.video_bitrate2 = video_bitrate2;
+
+	obj.outputenabled3 = outputenabled3;
+	obj.outputresolution3 = outputresolution3;
+	obj.video_bitrate3 = video_bitrate3;
+
+	obj.outputenabled4 = outputenabled4;
+	obj.outputresolution4 = outputresolution4;
+	obj.video_bitrate4 = video_bitrate4;
+
+	obj.hlsmanifest = hlsmanifest;
+	obj.fmp4manifest = fmp4manifest;
+	obj.dashmanifest = dashmanifest;
+	obj.publishpoint1 = publishpoint1;
+	obj.cdnusername1 = cdnusername1;
+	obj.cdnpassword1 = cdnpassword1;
+	
+	var postdata = JSON.stringify(obj);
+	
+	console.log(JSON.parse(postdata));
+
+	const url = "/api/v1/newsource";
+	const requestinfo = {
+	    method: 'POST',	    
+	    headers: {
+		'content-type':'application/json'
+	    },
+	    body: postdata
+	};
+
+	fetch(url, requestinfo)
+	    .then(function(data) {
+		console.log('Request success: ', data);
+		window.location.reload(true); 		
+	    }).then(function(error) {
+		console.log('Request failure: ', error);		
+	    });
+	
+    } else {
+	if (safe == 2) {
+	    alert("Bad Window Size Specified!");
+	}
+    }    
 });
 
-const abort_button = document.getElementById('abort_button');
 abort_button.addEventListener('click', function(e) {
     console.log('abort button was clicked');
     
--- webapp/server.js
+++ webapp/server.js
@@ -1,15 +1,25 @@
 console.log('Server-side code running');
 
-var os = require( 'os' );
-
+var os = require('os');
 var networkInterfaces = os.networkInterfaces( );
-
 const express = require('express');
-
+var bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
+
+app.use(bodyParser.json());
 
 // serve files from the public directory
 app.use(express.static('public'));
+
+const testFolder = '/var/tmp/configs';
+
+function getExtension(filename) {
+    var i = filename.lastIndexOf('.');
+    return (i < 0) ? '' : filename.substr(i);
+}
+
+function seconds_since_epoch(){ return Math.floor( Date.now() / 1000 ) }
 
 // start the express web server listening on 8080
 app.listen(8080, () => {
@@ -17,12 +27,117 @@ app.listen(8080, () => {
 });
 
 // serve the homepage
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+//app.get('/', (req, res) => {
+//    
+//    console.log('Active configurations: ', activeconfigurations);
+//    
+//    res.sendFile(__dirname + '/index.html');
+//});
+
+var activeconfigurations = 0;
+
+fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+	console.log(getExtension(file));
+	if (getExtension(file) == '.json') {
+	    activeconfigurations++;
+	}
+    });    
+});
+
+app.get('/get_configuration_count', (req, res) => {
+    res.send(JSON.stringify(activeconfigurations));
+});
+
+app.get('/get_configuration_data', (req, res) => {   
+});
+
+app.get('/get_control_page', (req, res) => {
+    var html = '';
+    var i;
+    var files = fs.readdirSync('/var/tmp/configs');
+    var listedfiles = 0;
+    
+    html += '<table>';
+    html += '<thead>';
+    html += '<tr class="header">';
+    html += '<th>Session ID<div>Session ID</div></th>';
+    html += '<th>Name<div>Name</div></th>';
+    html += '<th>Process Control<div>Process Control</div></th>';
+    html += '<th>Ingesting<div>Ingesting</div></th>';
+    html += '<th>Uptime<div>Uptime</div></th>';
+    html += '<th>Signal<div>Signal</div></th>';
+    html += '<th>Session Control<div>Session Control</div></th>';
+    html += '</tr>';
+    html += '</thead>';
+    html += '<tbody>';
+
+    files.forEach(file => {
+	console.log(getExtension(file));
+	if (getExtension(file) == '.json') {
+	    var configindex = listedfiles + 1;
+	    var fullfile = '/var/tmp/configs/'+file;
+	    var configdata = fs.readFileSync(fullfile, 'utf8');
+	    var words = JSON.parse(configdata);
+	    
+	    listedfiles++;
+	    console.log(listedfiles);
+
+	    html += '<tr>';
+	    html += '<td>' + configindex + '</td>';
+	    html += '<td>' + words.sourcename + '</td>';
+            html += '<td>';
+	    html += '<button id=\'start_button'+configindex+'\'>Start</button>';
+	    html += '<button id=\'stop_button'+configindex+'\'>Stop</button>';
+	    html += '<button id=\'reset_button'+configindex+'\'>Reset</button>';
+	    html += '</td>';
+	    html += '<td>ACTIVE</td>';
+	    html += '<td></td>';
+	    html += '<td>YES</td>';
+	    html += '<td>';
+	    html += '<button id=\'log'+configindex+'\' type=\'log\'>Show<br>Log</button>';
+	    html += '<button id=\'status'+configindex+'\' type=\'button\'>Show<br>Status</button>';
+	    html += '<button id=\'edit'+configindex+'\' type=\'edit\'>Update<br>Session</button>';
+	    html += '<button id=\'remove'+configindex+'\' type=\'remove\'>Remove<br>Session</button>';
+	    html += '</td>';
+	    html += '</tr>';
+	}
+    })
+    
+    html += '</tbody>';
+    html += '</table>';
+    
+    res.writeHead(200, {
+	'Content-Type': 'text/html',
+	'Content-Length': html.length,
+	'Expires': new Date().toUTCString()
+    });
+    res.end(html);
 });
 
 app.get('/get_interfaces', (req, res) => {    
     res.send(networkInterfaces);
+});
+
+app.post('/api/v1/newsource', (req, res) => {
+    console.log('received new source request');
+    console.log('body is ',req.body);
+
+    var nextconfig = '/var/tmp/configs/' + seconds_since_epoch() + '.json';
+    
+    fs.writeFile(nextconfig, JSON.stringify(req.body), (err) => {
+	if (err) {
+	    console.error(err);
+	    return;
+	};
+	console.log("File has been created");
+
+	activeconfigurations++;
+
+	console.log('updated active configurations: ', activeconfigurations);
+    });
+
+    res.send(req.body);    
 });
 
 app.post('/start_clicked', (req, res) => {
