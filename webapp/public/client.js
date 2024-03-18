6edf0bb7346d0f358a86c5176f138da2573f4354
cannonbeach <cannonbeachgoonie@gmail.com>
2024-03-18 11:18:12 -0700

    Uptime now correctly reflects the number of days

--- webapp/public/client.js
+++ webapp/public/client.js
@@ -864,10 +864,11 @@ function trigger_image_update()
 
 var toHHMMSS = (secs) => {
     var sec_num = parseInt(secs, 10)
+    var days    = Math.floor(sec_num / 86400)
     var hours   = Math.floor(sec_num / 3600) % 24
     var minutes = Math.floor(sec_num / 60) % 60
     var seconds = sec_num % 60
-    return [hours,minutes,seconds]
+    return [days,hours,minutes,seconds]
         .map(v => v < 10 ? "0" + v : v)
         .filter((v,i) => v !== "00" || i > 0)
         .join(":")
